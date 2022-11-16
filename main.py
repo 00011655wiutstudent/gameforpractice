88f1d4b9d0822d53997ccaf47aaeb7a63180d7b4
00011655wiutstudent <00011655wiutstudent@gmail.com>
2022-11-16 22:42:55 +0500

    the snake is getting bigger as it eats

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import time
 from pygame.locals import *
 import time
 
+SIZE = 40
 
 class Snake:
-    def __init__(self, parent_screen):
+    def __init__(self, parent_screen, length):
+        self.length = length
         self.parent_screen = parent_screen
         self.block = pygame.image.load("resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
+        self.x = [SIZE] *length
+        self.y = [SIZE] *length
         self.direction = "right"
     def draw(self):
         self.parent_screen.fill((255, 255, 255))
-        self.parent_screen.blit(self.block, (self.x, self.y))
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
 
     def move_up(self):
@@ -25,21 +28,24 @@ class Snake:
     def move_right(self):
         self.direction = "right"
     def walk(self):
+        for i in range(self.length-1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
         if self.direction == "right":
-            self.x += 10
+            self.x[0] += SIZE
         if self.direction == "left":
-            self.x -= 10
+            self.x[0] -= SIZE
         if self.direction == "down":
-            self.y += 10
+            self.y[0] += SIZE
         if self.direction == "up":
-            self.y = 10
+            self.y[0] -= SIZE
         self.draw()
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1000, 500))
+        self.surface = pygame.display.set_mode((1000, 800))
         self.surface.fill((255, 255, 255))
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, 6)
         self.snake.draw()
     def run(self):
         run = True
@@ -61,10 +67,9 @@ class Game:
                     run = False
 
             self.snake.walk()
-            time.sleep(0.2)
+            time.sleep(0.5)
 
 
 if __name__ == "__main__":
     game = Game()
     game.run()
-
