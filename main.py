0928d46ee197eda357029998a6d07ea052591351
00011655wiutstudent <00011655wiutstudent@gmail.com>
2022-11-16 16:10:09 +0500

    the body of the snake (block) was added and can move

--- main.py
+++ main.py
@@ -6,19 +6,34 @@ if __name__ == "__main__":
     pygame.init()
 
     surface = pygame.display.set_mode((1000, 500))
-    surface.fill((255, 255, 255))
+    block = pygame.image.load("resources/block.jpg").convert()
+    block_x = 100
+    block_y = 100
+    def draw_block():
+        surface.fill((255, 255, 255))
+        surface.blit(block, (block_x, block_y ))
+        pygame.display.flip()
 
-    block = pygame.image.load()
 
     pygame.display.update()
+
 
     run = True
 
     while run:
+        draw_block()
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_q:
                     run = False
+                if event.key == K_UP:
+                    block_y -=10
+                if event.key == K_DOWN:
+                    block_y +=10
+                if event.key == K_RIGHT:
+                    block_x +=10
+                if event.key == K_LEFT:
+                    block_x -=10
 
             elif event.type == QUIT:
                 run = False
